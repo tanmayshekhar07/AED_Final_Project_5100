b2006d048edd3afbdcfb3e8ae59417b758a80e8e
Tanmay Shekhar <81084072+tanmayshekhar07@users.noreply.github.com>
2022-12-06 20:53:52 -0500

    first_commit

---
+++ JavaApplication21/src/javaapplication21/LaLaLa.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication21;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author tanmayshekhar
+ */
+public class LaLaLa {
+      private String name;
+    private int ID;
+    private int age;
+    private String gender;
+    private String date;
+    private double level;
+    private String Info;
+    private String Position;
+    private long Phone;
+    private String email;
+    private ImageIcon img;
+
+}
+
+
